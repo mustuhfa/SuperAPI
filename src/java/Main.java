362b58b2ccce580cf2a29c61408dd2a5d4fc397b
Mustafa Sezgin <mov.rdx@gmail.com>
2016-10-30 22:30:06 +0100

    Setup initial workspace

---
+++ src/java/Main.java
@@ -0,0 +1,29 @@
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class Main extends Application<Configuration> {
+
+    public static void main(String[] args) throws Exception {
+        new Main().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "SuperAPI";
+    }
+
+    @Override
+    public void initialize(Bootstrap<Configuration> bootstrap) {
+        
+    }
+
+    @Override
+    public void run(Configuration configuration,
+                    Environment environment) {
+
+
+    }
+
+}
